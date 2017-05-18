ab6ef7642c3822b93530791fbcc7b9ff28f65783
Smart_UA <oleksandr.patuk@gmail.com>
2017-05-18 20:02:42 +0300

    init commit

---
+++ app/src/main/java/home/smart/cn/ua/androidtest/MainActivity.java
@@ -0,0 +1,13 @@
+package home.smart.cn.ua.androidtest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layoutLogin);
+    }
+}
